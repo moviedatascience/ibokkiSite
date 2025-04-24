ec9025734703618ded338f63daa355d4e00c7d21
MDS <ericdavidsmith91@gmail.com>
2025-04-24 11:38:46 -0400

    added in nginx, chat functionality

--- ibokki/asgi.py
+++ ibokki/asgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
+from home.consumers import ChatConsumer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ibokki.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            path("ws/chat/", ChatConsumer.as_asgi()),
+        ])
+    ),
+})
